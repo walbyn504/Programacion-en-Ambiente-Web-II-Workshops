6723e61d0fb8a684d9dbd12324b9e8c748fdd14c
walbyn <walbyngonzalez@gmail.com>
2026-02-12 14:17:08 -0600

    Se modifico el modelo de course (se agregaron mas campos)

--- Workshop3/SERVER/models/course.js
+++ Workshop3/SERVER/models/course.js
@@ -2,13 +2,22 @@ const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
     name: {
-        required: true,
-        type: String
+        type: String,
+        required: true
     },
-    credits: {
-        required: true,
-        type: Number
+    code: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    professorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Professor',
+        required: true
     }
-})
+});
 
-module.exports = mongoose.model('Course', courseSchema)
+module.exports = mongoose.model('Course', courseSchema);
