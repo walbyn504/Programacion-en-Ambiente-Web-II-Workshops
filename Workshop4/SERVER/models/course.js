1b4b2b730dce8e03b272c60627e639a04de205e9
walbyn <walbyngonzalez@gmail.com>
2026-02-25 13:56:26 -0600

    Se crea el modelo de cursos

---
+++ Workshop4/SERVER/models/course.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    credits: {
+        required: true,
+        type: Number
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    professorId: {
+        //Indentificador unico y relaciona documentos
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Professor',
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Course', courseSchema);
